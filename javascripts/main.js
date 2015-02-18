d8e494453c0acd8ac7127c0df7143792d1ee0a55
Marco Musso <marco.musso@kmc.torino.it>
2015-02-18 20:43:58 +0100

    Added CodeFlower as an example

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,4 @@
-console.log('This would be the main JS file.');
+$("document").ready(function() {
+  var myFlower = new CodeFlower("#codeflower", 600, 400);
+  myflower.update('data/bookkeeping.json');
+});
